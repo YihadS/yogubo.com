c0edda13043f2ef0feae3f245292bc351a9794a3
Yihad Salek Villalba <72087335+YihadS@users.noreply.github.com>
2021-11-17 10:44:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var boton1 = document.getElementById('boton1');
+var boton2 = document.getElementById('boton2');
+var boton3 = document.getElementById('boton3');
+
+function cambio1(){
+    boton1.style.display="block";
+}
+
+function cambio1f(){
+    boton1.style.display="none";
+}
+
+function cambio2(){
+    boton2.style.display="block";
+}
+
+function cambio2f(){
+    boton2.style.display="none";
+}
+
+function cambio3(){
+    boton3.style.display="block";
+}
+
+function cambio3f(){
+    boton3.style.display="none";
+}
+
