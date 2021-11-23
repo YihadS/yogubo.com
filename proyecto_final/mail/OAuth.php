1431470fcd2c32aa0117a0e766012e4b7edaf159
Yihad Salek Villalba <72087335+YihadS@users.noreply.github.com>
2021-11-23 12:29:35 -0400

    Update OAuth.php

--- proyecto_final/mail/OAuth.php
+++ proyecto_final/mail/OAuth.php
@@ -1,4 +1,20 @@
-* @note      This program is distributed in the hope that it will be useful - WITHOUT
+<?php
+
+/**
+ * PHPMailer - PHP email creation and transport class.
+ * PHP Version 5.5.
+ *
+ * @see       https://github.com/PHPMailer/PHPMailer/ The PHPMailer GitHub project
+ *
+ * @author    Marcus Bointon (Synchro/coolbru) <phpmailer@synchromedia.co.uk>
+ * @author    Jim Jagielski (jimjag) <jimjag@gmail.com>
+ * @author    Andy Prevost (codeworxtech) <codeworxtech@users.sourceforge.net>
+ * @author    Brent R. Matzelle (original founder)
+ * @copyright 2012 - 2020 Marcus Bointon
+ * @copyright 2010 - 2012 Jim Jagielski
+ * @copyright 2004 - 2009 Andy Prevost
+ * @license   http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ * @note      This program is distributed in the hope that it will be useful - WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.
  */
@@ -121,3 +137,4 @@ class OAuth
         );
     }
 }
+
